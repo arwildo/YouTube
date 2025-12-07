c6cabb0bda57fa79b4b4d41d79c38bc1413cc069
Arwildo <arwildo@gmail.com>
2025-12-07 15:51:34 +0700

    feat(pohon-natal): tambahkan bintang dan juga bola

--- pohon-natal/main.py
+++ pohon-natal/main.py
@@ -1,13 +1,23 @@
+import random
 
-tinggi_daun = 9
+tinggi_daun = 10
 tinggi_batang = 3
+
+POLA_DAUN = ["^", "^", "^", "o"]
 
 for i in range(tinggi_daun):
     spasi = " " * (tinggi_daun - i)
-    daun = "*" * (2 * i + 1)
-    print(spasi + daun)
+    if i == 0: 
+        print(spasi + "*")
+    else:
+        baris_daun = []
+        for k in range(2 * i + 1):
+            pola_sekarang = random.choice(POLA_DAUN)
+            baris_daun.append(pola_sekarang)
+        print(spasi + ''.join(baris_daun))
+
 
 for j in range(tinggi_batang):
     spasi = " " * (tinggi_daun - 1)
-    batang = "|" * 3
+    batang = "| " * 2
     print(spasi + batang)
