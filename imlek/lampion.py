d4becbd3a0d8c5c4c2d449405bd96de228bf4349
Arwildo <arwildo@gmail.com>
2026-02-10 21:48:29 +0700

    feat: tambah project implek lampion berjejer

---
+++ imlek/lampion.py
@@ -0,0 +1,30 @@
+def tali(panjang=80):
+    print('_' * panjang)
+
+def lampion():
+    spasi = '  '
+    print(spasi + ' _|_')
+    print(spasi + '|   |')
+    print(spasi + '|_x_|')
+    print(spasi + "  '  ")
+
+def lampion_berjejer(jumlah=8, spasi=4):
+    for i in range(jumlah):
+        print(' ' * spasi + ' _|_ ', end='')
+    print()
+    
+    for i in range(jumlah):
+        print(' ' * spasi + '|   |', end='')
+    print()
+    
+    for i in range(jumlah):
+        print(' ' * spasi + '|_x_|', end='')
+    print()
+
+    for i in range(jumlah):
+        print(' ' * spasi + "  '  ", end='')
+    print()
+    
+if __name__ == "__main__":
+    tali()
+    lampion_berjejer()
